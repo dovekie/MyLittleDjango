c4a2694010708eca03f9c479e6e5699064732132
dovekie <dovekie@gmail.com>
2015-10-16 11:31:09 -0700

    Building out the admin tool

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline): # sets up Choice to be edited within Question (three choices for each question)
+	# above, TabularInline makes the choices into a table-like form
+	# another option is StackedInline, which is less compressed.
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	# fields = ['pub_date', 'question_text'] # this determines the order in which the fields are displayed on the /admin/ page
+	fieldsets = [ # this puts them in order and groups them by (Title, {'fields': ['list', 'of', 'fields'], 'classes': [html classes]})
+		(None, {'fields': ['question_text']}),
+		('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}) # 'collapse' makes a field hide itself until clicked
+	]
+	inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
